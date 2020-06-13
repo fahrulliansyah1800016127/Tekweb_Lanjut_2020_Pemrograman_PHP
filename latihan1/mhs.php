e8c65a5fa23aa26df021177b4163a460e286d4fa
fahrullyn <fahrulliansyah1800016127@webmail.uad.ac.id>
2020-06-13 17:00:18 +0700

    first commit

---
+++ latihan1/mhs.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>mhs</title>
+</head>
+
+<body>
+<b><center>MUHAMMAD ZULKIPLI FAHRULLIANSYAH 1800016127</center></b>
+<BR> 
+<b><center>TEKWEB LANJUT KELAS C </center></b>
+<BR>
+<b><center>PRAKTIKUM TEKWEB D</center>
+<BR>
+<BR>
+
+
+<table border="1" align="center">
+<tr>
+     <td>NIM</td>
+     <td>:</td>
+     <td> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"] ?></td>
+</tr>
+<tr>
+     <td>Jenis Kelamin</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"] ?></td>
+</tr>
+<tr>
+     <td>Program Studi</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"] ?></td>
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="mhs.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
---
+++ latihan2/mhs.php
@@ -0,0 +1,51 @@
+<html>
+<body>
+
+
+<b><center>MUHAMMAD ZULKIPLI FAHRULLIANSYAH 1800016127</center></b>
+<BR>
+<b><center>TEKWEB LANJUT KELAS C </center></b>
+<BR>
+<b><center>PRAKTIKUM TEKWEB D</center>
+<BR>
+<BR>
+
+
+<table border="1" align="center">
+<tr>
+     <td>NIM</td>
+     <td>:</td>
+     <td> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>JENIS KELAMIN</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>ALAMAT</td>
+     <td>:</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td>PROGRAM STUDI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>
+     
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="mhs.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
